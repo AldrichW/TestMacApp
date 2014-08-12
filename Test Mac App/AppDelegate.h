ec002971c3809a1998e8449947b1f0e4efd8aba7
Aldrich Wingsiong <awingsiong@apple.com>
2014-08-12 16:08:33 -0700

    All done!

--- Test Mac App/AppDelegate.h
+++ Test Mac App/AppDelegate.h
@@ -7,7 +7,7 @@
 //
 
 #import <Cocoa/Cocoa.h>
-@interface AppDelegate : NSObject <NSApplicationDelegate>
+@interface AppDelegate: NSObject <NSApplicationDelegate>
 
 @property (nonatomic ,readonly) BOOL iHopeThisWorks;
 @property (nonatomic, readonly) NSString *myName;
